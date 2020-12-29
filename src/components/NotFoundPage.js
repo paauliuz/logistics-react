bf1f771d7a12ac09f342b2d02e17df105e84bb1b
Paulius <paulius.sink@gmail.com>
2020-12-29 16:13:03 -0300

    Added 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
 
 const NotFoundPage = () => (
-    <div>
-        Not Found 404 <Link to="/">Home</Link>
+    <div className="page-wrapper">
+        <Header />
+        <div className='content-container not-found-container'>
+            <h1 classname="not-found-title">Not Found 404</h1>
+        </div>
+        <Footer />
     </div>
 )
 
